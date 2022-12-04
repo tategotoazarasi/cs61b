ea06e9cfa54562820d8c5f700dcc6e5108c1845c
Tategoto Azarasi <2724167997@qq.com>
2022-12-04 21:26:22 +0800

    ReadPlanets
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -4,4 +4,21 @@ public class NBody {
         in.readInt();
         return in.readDouble();
     }
+
+    public static Planet[] readPlanets(String planetsTxtPath) {
+        In in = new In(planetsTxtPath);
+        int n = in.readInt();
+        in.readDouble();
+        Planet[] ans = new Planet[n];
+        for (int i = 0; i < n; i++) {
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            ans[i] = new Planet(xP, yP, xV, yV, m, img);
+        }
+        return ans;
+    }
 }
